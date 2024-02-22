5e98a5ed54b2b155de15ffcc34ced8d3750aed68
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-22 12:20:53 +0300

    Take input from user and store it in state

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,12 +1,40 @@
-import './ItemList.style.css'
+import { useState, useEffect } from "react";
+import "./ItemList.style.css";
 
 function ItemList() {
+    const [items, setItems] = useState([]);
 
-  return (
-    <>
-      
-    </>
-  )
+    const takeInput = (event) => {
+        setItems((prevItems) => [...prevItems, event.target.value]);
+        event.target.value = "";
+    };
+
+    useEffect(() => {
+        const keyDownHandler = (event) => {
+            preventDefault();
+            if (event.key === "Enter") {
+                event.preventDefault();
+
+                takeInput(event);
+            }
+        };
+
+        document.addEventListener("keydown", keyDownHandler);
+
+        return () => {
+            document.removeEventListener("keydown", keyDownHandler);
+        };
+    }, []);
+
+    return (
+        <>
+            <input type="text" />
+
+            <div className="item-list"></div>
+
+            <div className="item-info"></div>
+        </>
+    );
 }
 
-export default ItemList
+export default ItemList;
