916bb46bb9f11f846f98c0ea84b6fe156de5a335
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-22 22:04:19 +0300

    Wrap components in div

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import './App.css'
-import ItemList from './Components/ItemList/ItemList'
-import Title from './Components/Title/Title'
+import "./App.css";
+import ItemList from "./Components/ItemList/ItemList";
+import Title from "./Components/Title/Title";
 
 function App() {
-
-  return (
-    <>
-      <Title></Title>
-      <ItemList></ItemList>
-    </>
-  )
+    return (
+        <div className="app-component">
+            <Title></Title>
+            <ItemList></ItemList>
+        </div>
+    );
 }
 
-export default App
+export default App;
