14601f0e9b29513f9c24f6aa87c38262d83b5e02
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-22 22:04:49 +0300

    Add filter functionality

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,17 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./ItemList.style.css";
+import Item from "../Item/Item";
 
 function ItemList() {
     const [items, setItems] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [filterState, setFilterState] = useState("all");
+
+    useEffect(() => {
+        if (filterState === "all") {
+            setFilteredItems(items);
+        }
+
+        if (filterState === "active") {
+            setFilteredItems(items.filter((obj) => !obj.isCompleted));
+        }
+
+        if (filterState === "completed") {
+            setFilteredItems(items.filter((obj) => obj.isCompleted));
+        }
+    }, [filterState, items]);
+
 
     const takeInput = (event) => {
-        setItems((prevItems) => [...prevItems, event.target.value]);
+        const object = {
+            taskDescription: event.target.value,
+            isCompleted: false,
+            isRemoved: false,
+        };
+
+        setItems((prevItems) => [...prevItems, object]);
         event.target.value = "";
     };
 
     useEffect(() => {
         const keyDownHandler = (event) => {
-            preventDefault();
             if (event.key === "Enter") {
                 event.preventDefault();
 
@@ -26,14 +49,40 @@ function ItemList() {
         };
     }, []);
 
+
     return (
-        <>
+        <div className="item-list-component">
             <input type="text" />
 
-            <div className="item-list"></div>
+            {filteredItems.map((item) => {
+                return (
+                    <Item
+                        key={item.taskDescription}
+                        item={item}
+                        items={items}
+                        setItems={setItems}
+                    ></Item>
+                );
+            })}
 
-            <div className="item-info"></div>
-        </>
+            <div className="item-info">
+                <div className="number-of-items">{items.filter((obj) => !obj.isCompleted).length} left</div>
+
+                <div className="item-buttons">
+                    <button onClick={() => setFilterState("all")}>All</button>
+                    <button onClick={() => setFilterState("active")}>
+                        Active
+                    </button>
+                    <button onClick={() => setFilterState("completed")}>
+                        Completed
+                    </button>
+                </div>
+
+                <div className="clear-completed-button">
+                    <button onClick={() => setItems(items.filter((obj) => !obj.isCompleted))}>Clear completed</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
